368e42c87632b5774858335a57656d5a18f094f6
YesicaNaranjo <yesicanaranjos@gmail.com>
2023-05-04 07:34:39 -0500

    cambio de computadora e inicializacion de git

---
+++ main.js
@@ -0,0 +1,83 @@
+/*          CARRITO DE COMPRAS
+Estructura para un carrito de compras
+Calcula el costo total de productos agregados a un carrito de compras, valor con costo por unidad y descuentos.*/
+
+//Declaración de variables:
+const greeting = alert("Bienvenido a tu tienda virtual, a continuación contabilizaremos tu carrito de compras.");
+let totalValue = 0;
+let productValue = 0;
+let quantity = 0;
+let price = 0;
+
+//Declaración de objetos y clases:
+class Login{
+    constructor(user, password){
+        this.user = usuario; 
+        this.password = password;
+    }
+}
+
+class Client{
+    constructor(name, lastname, age){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+}
+const juan = new Client ("Juan", "Laverde", 25);
+console.log (juan);
+
+class Product{
+    constructor(name, quantity, price){
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
+//Declaración de arreglos:
+const arrayVegetables = ["tomatoes", "onion", "paprika", "eggplant"];
+const arrayQuantities = [];
+const arrayPrice = [120, 150, 400, 250];
+
+//Funcion que calcula el valor total de la compra
+
+function purchaseValue(productValue){
+
+//Con el método push el cliente adiciona las camtidades de los productos elegidos en su carrito.
+    for (let i=0; i<4; i++){
+    arrayQuantities.push(prompt("Ingresa la cantidad de " + arrayVegetables[i] + " de tu compra:"));
+    }
+    console.log(arrayQuantities);
+
+   for(let i=0; i<arrayVegetables.length; i++){
+    var productValue =  arrayQuantities[i] * arrayPrice[i];
+    totalValue = totalValue + productValue;
+    }
+    console.log("Total value " + totalValue);
+    return totalValue ;
+}    
+let resultado = purchaseValue();
+alert("El costo total de tu compra es de : $ " + resultado );
+purchaseValue();
+
+const thanks = alert("¡Gracias por tu compra!");
+
+
+/*
+    //Funcion que calcula el valor total de la compra
+    function purchaseValue(product,quantity){
+        producto = parseInt(prompt("Ingrese el valor del producto que deseas comprar ($) :"));
+        console.log(producto, typeof producto);
+        while(producto <= 0){
+            alert("Recuerda que el valor ingresado debe ser númerico y mayor a 0.");
+            producto = parseInt(prompt("Ingrese el valor del producto que deseas comprar ($) :"));
+       }
+        cantidad = parseInt(prompt("Ingresa el número de unidades a comprar: "));
+        //valorP = alert("El valor total a pagar de este producto es: $ " + producto * cantidad); 
+        return  producto * cantidad ;
+    }    
+    let resultado = valorTotal();
+    alert("El costo total del producto es : $ " + resultado );
+    valorTotal();
+    */
